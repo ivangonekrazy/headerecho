f9b3ae2c31c04123bce77f2753449f5664612c8f
Ivan Tam <ivan@duraflame.(none)>
2011-01-23 00:07:06 -0800

    - scaled back to a simpler method-based WSGI server   rather than a more complex class-based one - the served up HTML page will autolaunch   a test for the X-Requested-With header

--- headerecho.py
+++ headerecho.py
@@ -1,71 +1,26 @@
-import cgi
+def headerEcho(environ, start_response):
+  try:
+    query = environ.get('QUERY_STRING')
+    path  = environ.get('PATH_INFO')
+    if len(query) == 0 and path == '/': 
+      print 'returning html'
+      start_response('200 OK', [('Content-type', 'text/html')])
+      return open('headerecho.html').readlines()
 
-class HeaderEcho:
-  """ Echo the requests headers back
-      in the response
-  """
+    req_header = environ.get( 'HTTP_%s' % query.upper().replace('-','_') )
+    if req_header is None:
+      start_response('404 Not Found', [('Content-type', 'text/plain')])
+      return ['Request header %s not defined.\n' % query]
 
-  resp_code = {
-    'OK'       : '200 OK',
-    'NOT_FOUND': '404 Not Found',
-    'ERROR'    : '500 Server Error'
-  }
-
-  default_resp_headers = [
-    ('Content-type', 'text/plain')
-  ]
-
-  def __init__(self, environ, start_response):
-    self.environ = environ
-    self.start_response = start_response
-
-  def __iter__(self):
-    """ Takes a request like <server>?header=REMOTE_HOST
-        and returns the value of that header if found
-        returns 404 if not found
-    """
-    resp_headers = []
-
-    try:
-      target = self.query_params().get('header')
-      if target is None:
-        self.start_response(self.resp_code['NOT_FOUND'], self.default_resp_headers)
-        yield 'No target request header defined.\n'
-
-      req_header = self.environ.get(target)
-      if req_header is None:
-        self.start_response(self.resp_code['NOT_FOUND'], self.default_resp_headers)
-        yield 'Request header %s not defined.\n' % target
-
-      self.start_response(self.resp_code['OK'], self.default_resp_headers + resp_headers)
-      yield 'Request header %s set to %s.\n' % (target, req_header)
-    except:
-      self.start_response(self.resp_code['ERROR'], self.default_resp_headers)
-      yield 'Server Error\n'
+    start_response('200 OK', [('Content-type', 'application/json')])
+    return ['{"%s" : "%s"}\n' % (query, req_header)]
+  except Exception, e:
+    print e
       
-
-  def query_params(self, environ=None):
-    """ Converts the URL query string into a dict
-    """
-    params = {}
-
-    if environ is None:
-      environ = self.environ
-
-    query_string = environ.get('QUERY_STRING', '')
-    param_strings = query_string.split('&')
-
-    for param_string in param_strings:
-      k,v = param_string.split('=')
-      params[k] = v
-
-    return params
-
-
 if __name__ == '__main__':
   from wsgiref import simple_server
   PORT = 8000
-  print 'Server listening at %s' % ( PORT )
   httpd = simple_server.WSGIServer( ('', PORT), simple_server.WSGIRequestHandler )
-  httpd.set_app(HeaderEcho)
+  httpd.set_app(headerEcho)
+  print 'Server listening at %s' % ( PORT )
   httpd.serve_forever()
