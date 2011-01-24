a80a66b2f72ef7a97e4f959aa913fefde5f661a9
Ivan Tam <ivan@duraflame.(none)>
2011-01-23 17:25:57 -0800

    making this work with modwsgi

--- headerecho.py
+++ headerecho.py
@@ -17,31 +17,51 @@
   -->
     {"X-Requested-With" : "XmlHttpRequest"}
 """
+import sys, os.path
+
+# only do stdout redirection if we're not running from
+# the command-line
+if __name__ != '__main__':
+  sys.stdout = sys.stderr
 
 def application(environ, start_response):
   try:
+    script_filename = environ.get('SCRIPT_FILENAME')
     query = environ.get('QUERY_STRING')
     path  = environ.get('PATH_INFO')
 
     # if there isn't a querystring, return the client-side
     # test page.
-    if len(query) == 0 and path == '/': 
-      start_response('200 OK', [('Content-type', 'text/html')])
-      return open('headerecho.html').readlines()
+    if len(query) == 0 and path in ['/','']: 
+      html_path = os.path.join( os.path.dirname(script_filename), 'headerecho.html')
+      return response(
+             start_response,
+             '200 OK',
+             ''.join( open(html_path).readlines() ),
+             'text/html'
+      )
 
     # return the header provided in the query string 
     # iff the header is in the environ dict
     req_header = environ.get( 'HTTP_%s' % query.upper().replace('-','_') )
     if req_header is None:
-      start_response('404 Not Found', [('Content-type', 'text/plain')])
-      return ['Request header %s not defined.\n' % query]
+      return response('404 Not Found', 'Request header %s not defined.\n' % query, start_response)
 
     # build and return the JSON response 
-    start_response('200 OK', [('Content-type', 'application/json')])
-    return ['{"%s" : "%s"}\n' % (query, req_header)]
+    return response(
+           start_response,
+           '200 OK',
+           '{"%s" : "%s"}\n' % (query, req_header),
+           'application/json'
+    )
   except Exception, e:
-    start_response('500 Server Error', [('Content-type', 'text/plain')])
-    return ['Server error: %s' % str(e)]
+    return response(start_response, '500 Server Error', 'Server error: %s' % str(e))
+
+def response(start_response, status, resp_string, resp_type='text/plain'):
+    resp_len = str(len(resp_string))
+    resp_headers = [('Content-type', resp_type), ('Content-length', resp_len) ]
+    start_response(status, resp_headers)
+    return [resp_string]
       
 # run standalone if we're invoked from the command-line
 if __name__ == '__main__':
@@ -49,5 +69,6 @@ if __name__ == '__main__':
   PORT = 8000
   httpd = simple_server.WSGIServer( ('', PORT), simple_server.WSGIRequestHandler )
   httpd.set_app(application)
-  print 'headerEcho server listening at %s' % ( PORT )
+  print 'headerecho server listening at %s' % ( PORT )
+  print 'ctrl-c to kill me ...'
   httpd.serve_forever()
